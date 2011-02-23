691bea9b3177bab5bb9fb37647e0ebbd92756b16
Alex Wayne <alex@beautifulpixel.com>
2011-02-23 09:19:08 -0800

    Updating static assets...

--- javascripts/spec/bullet_spec.js
+++ javascripts/spec/bullet_spec.js
@@ -1,3 +1,25 @@
 (function() {
-
+  describe('Bullet', function() {
+    var bullet;
+    bullet = null;
+    beforeEach(function() {
+      return bullet = new Bullet(v(10, 10), v(20, 20));
+    });
+    it('should be at position', function() {
+      expect(bullet.pos.x).toEqual(10);
+      return expect(bullet.pos.y).toEqual(10);
+    });
+    it('should be pointed toward target', function() {
+      expect(bullet.vel.angle).not.toEqual(45);
+      expect(bullet.vel.angle).toBeLessThan(50);
+      return expect(bullet.vel.angle).toBeGreaterThan(40);
+    });
+    return it('should expire when it slows down', function() {
+      Game.game.stage.gameObjects.push(bullet);
+      expect(Game.game.stage.gameObjects.indexOf(bullet)).toBeGreaterThan(-1);
+      bullet.vel.length = 49;
+      bullet.update();
+      return expect(Game.game.stage.gameObjects.indexOf(bullet)).toEqual(-1);
+    });
+  });
 }).call(this);
