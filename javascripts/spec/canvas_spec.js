f9a93fe55bf247f1b209f710b96cfd59973871c6
Alex Wayne <alex@beautifulpixel.com>
2011-02-22 23:53:38 -0800

    Updating static assets...

--- javascripts/spec/canvas_spec.js
+++ javascripts/spec/canvas_spec.js
@@ -18,14 +18,14 @@
         gamePos = this.canvas.toGamePos(v(300, 300));
         expect(gamePos.length).toEqual(0);
         gamePos = this.canvas.toGamePos(v(60, 300));
-        expect(gamePos.x).toEqual(-160);
+        expect(gamePos.x).toEqual(-80);
         expect(gamePos.y).toEqual(0);
         gamePos = this.canvas.toGamePos(v(300, 60));
         expect(gamePos.x).toEqual(0);
-        expect(gamePos.y).toEqual(160);
+        expect(gamePos.y).toEqual(80);
         gamePos = this.canvas.toGamePos(v(540, 540));
-        expect(gamePos.x).toEqual(160);
-        return expect(gamePos.y).toEqual(-160);
+        expect(gamePos.x).toEqual(80);
+        return expect(gamePos.y).toEqual(-80);
       });
     });
   });
